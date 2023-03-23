99bb3558bd5142367d599589b5990ceb120e61b7
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-03-23 09:47:20 +0300

    able to delete todo

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,4 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
+import EditTodo from "./EditTodo";
+
 
 const ListTodos = () => {
     const [todos, setTodos] = useState([])
@@ -44,12 +46,12 @@ const ListTodos = () => {
       </tr>
     </thead>
     <tbody>
-      
+     
 
       {todos.map(todo => (
          <tr key={todo.todo_id}>
             <td>{todo.description}</td>
-            <td>Edit</td>
+            <td><EditTodo todo ={todo} /></td>
             <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
          </tr>
       ))}
